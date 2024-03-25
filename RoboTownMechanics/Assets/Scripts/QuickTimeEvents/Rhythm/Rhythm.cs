8582e9391601c17434568df5e5c545a3cc78520a
Olli Appelt <70954091+DerPanzerFaust@users.noreply.github.com>
2024-03-25 10:58:19 +0100

    Updated Rhythm QTE
    Fixed:
    When player presses button the button.sprite plays animation.
    
    Added:
    New UI sprites for cirkel and greenzone.

--- RoboTownMechanics/Assets/Scripts/QuickTimeEvents/Rhythm/Rhythm.cs
+++ RoboTownMechanics/Assets/Scripts/QuickTimeEvents/Rhythm/Rhythm.cs
@@ -7,72 +7,79 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 using UnityEngine.UI;
 
-public class Rhythm : MonoBehaviour
+namespace QuickTime.Rhythm
 {
-    //--------------------Private--------------------//
-    [SerializeField]
-    private Image _rhythmCirkel;
-    [SerializeField]
-    private float _tempo;
-    [SerializeField]
-    private int _targetWidth;
-    [SerializeField]
-    private int _targetHeight;
-    [SerializeField]
-    private int _originalWidth;
-    [SerializeField]
-    private int _originalHeight;
-    [SerializeField]
-    private int _bufferWidth;
-    [SerializeField]
-    private int _bufferHeight;
-    private InputComponent _inputComponent;
-
-    //--------------------Public--------------------//
-    public Action OnFailQuickTime;
-
-
-    //--------------------Functions--------------------//
-    private void Start()
+    public class Rhythm : MonoBehaviour
     {
-        _inputComponent = InputComponent.Instance;
-        _inputComponent.Interact.performed += InteractPressed;
+        //--------------------Private--------------------//
+        [SerializeField]
+        private Image _rhythmCirkel;
+        [SerializeField]
+        private float _tempo;
+        [SerializeField]
+        private int _targetWidth;
+        [SerializeField]
+        private int _targetHeight;
+        [SerializeField]
+        private int _originalWidth;
+        [SerializeField]
+        private int _originalHeight;
+        [SerializeField]
+        private int _bufferWidth;
+        [SerializeField]
+        private int _bufferHeight;
+        [SerializeField]
+        private Animation ButtonPress;
 
-        OnFailQuickTime += FailedQuickTime;
-    }
+        private InputComponent _inputComponent;
+        //--------------------Public--------------------//
+        public Action OnFailQuickTime;
 
-    private void Update()
-    {
-        MakeRhythm();
-    }
 
-    private void MakeRhythm()
-    {
-        _rhythmCirkel.rectTransform.sizeDelta -= new Vector2(_targetWidth, _targetHeight) * _tempo * Time.deltaTime;
-
-        if (_rhythmCirkel.rectTransform.sizeDelta.magnitude <= new Vector2(_targetWidth, _targetHeight).magnitude)
+        //--------------------Functions--------------------//
+        private void Start()
         {
-            Debug.Log("Too late!");
-            OnFailQuickTime.Invoke();
-        }
-    }
+            _inputComponent = InputComponent.Instance;
+            _inputComponent.Interact.performed += InteractPressed;
 
-    private void InteractPressed(InputAction.CallbackContext context)
-    {
-        if (_rhythmCirkel.rectTransform.sizeDelta.magnitude >= new Vector2(_targetHeight, _targetWidth).magnitude &&
-            _rhythmCirkel.rectTransform.sizeDelta.magnitude <= new Vector2(_bufferHeight, _bufferWidth).magnitude)
-        {
-            _rhythmCirkel.rectTransform.sizeDelta = new Vector2(_originalWidth, _originalHeight);
-            Debug.Log("On point!");
+            OnFailQuickTime += FailedQuickTime;
         }
-        else if (_rhythmCirkel.rectTransform.sizeDelta.magnitude >= new Vector2(_bufferHeight, _bufferWidth).magnitude)
-        {
-            Debug.Log("Too early!");
-        }
-    }
 
-    private void FailedQuickTime()
-    { 
-        //Hier komt de code voor wat er gebeurd als de speler de QTE faalt. Exit en wegschieten in ragdoll.
+        private void Update()
+        {
+            MakeRhythm();
+        }
+
+        private void MakeRhythm()
+        {
+            _rhythmCirkel.rectTransform.sizeDelta -= new Vector2(_targetWidth, _targetHeight) * _tempo * Time.deltaTime;
+
+            if (_rhythmCirkel.rectTransform.sizeDelta.magnitude <= new Vector2(_targetWidth, _targetHeight).magnitude)
+            {
+                Debug.Log("Too late!");
+                OnFailQuickTime.Invoke();
+            }
+        }
+
+        private void InteractPressed(InputAction.CallbackContext context)
+        {
+            if (_rhythmCirkel.rectTransform.sizeDelta.magnitude >= new Vector2(_targetHeight, _targetWidth).magnitude &&
+                _rhythmCirkel.rectTransform.sizeDelta.magnitude <= new Vector2(_bufferHeight, _bufferWidth).magnitude)
+            {
+                _rhythmCirkel.rectTransform.sizeDelta = new Vector2(_originalWidth, _originalHeight);
+                Debug.Log("On point!");
+            }
+            else if (_rhythmCirkel.rectTransform.sizeDelta.magnitude >= new Vector2(_bufferHeight, _bufferWidth).magnitude)
+            {
+                Debug.Log("Too early!"); //Hier komt wat er gebeurt als de speler te vroeg drukt
+            }
+
+            ButtonPress.Play();
+        }
+
+        private void FailedQuickTime()
+        {
+            //Hier komt de code voor wat er gebeurd als de speler de QTE faalt. Exit en wegschieten in ragdoll.
+        }
     }
 }

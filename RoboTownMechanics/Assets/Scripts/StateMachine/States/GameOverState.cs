7567ca358cb1155cb5531a6a4c75903b7b61f42f
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2024-04-11 10:41:07 +0200

    Game Timer
    The script now activates a canvas on the screen, so that player has a visible feedback that the game is over
    
    Changed : feature-gametimer.unity, GameOverState.cs

--- RoboTownMechanics/Assets/Scripts/StateMachine/States/GameOverState.cs
+++ RoboTownMechanics/Assets/Scripts/StateMachine/States/GameOverState.cs
@@ -1,11 +1,17 @@
 using StateMachines.States;
 using UnityEngine;
+using MenuHandler;
 
 public class GameOverState : State
 {
+    //--------------------Private--------------------//
+    private MenuManager _menuManager;
+
     //--------------------Functions--------------------//
     protected override void OnEnter()
     {
+        _menuManager = MenuManager.Instance;
+        _menuManager.OpenMenu("GameOver");
         Debug.Log("GameOver!");
     }
 

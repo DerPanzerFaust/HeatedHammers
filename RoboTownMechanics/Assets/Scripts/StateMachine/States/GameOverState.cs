add46626d9f068b63d49fefc99b8e63251cb94c9
Thom Koper <70897090+schlorp@users.noreply.github.com>
2024-05-23 10:18:54 +0200

    added blur to gameover
    blur also added to gameover screen
    
    changed:
    GameOverState.cs

--- RoboTownMechanics/Assets/Scripts/StateMachine/States/GameOverState.cs
+++ RoboTownMechanics/Assets/Scripts/StateMachine/States/GameOverState.cs
@@ -1,6 +1,9 @@
 using StateMachines.States;
 using MenuHandler;
 using PauseHandler;
+using PostProcessing.Profiles;
+using UnityEngine.Rendering;
+using UnityEngine;
 
 public class GameOverState : State
 {
@@ -8,15 +11,22 @@ public class GameOverState : State
     private MenuManager _menuManager;
     private PauseSystem _pauseSystem;
 
+    private VolumeProfileLibrary _profileLibrary;
+    private Volume _currentSceneVolume;
+
     //--------------------Functions--------------------//
     protected override void OnEnter()
     {
         _menuManager = MenuManager.Instance;
         _menuManager.OpenMenu("GameOver");
 
+        _profileLibrary = VolumeProfileLibrary.Instance;
+
+        _currentSceneVolume = GameObject.FindObjectOfType<Volume>();
+        _currentSceneVolume.profile = _profileLibrary.VolumeProfiles[1];
+
         _pauseSystem = PauseSystem.Instance;
         _pauseSystem.PausingGame();
-        
     }
 
     protected override void OnUpdate()
@@ -26,5 +36,7 @@ public class GameOverState : State
     protected override void OnExit()
     {
         _menuManager.CloseMenu("GameOver");
+
+        _currentSceneVolume.profile = _profileLibrary.VolumeProfiles[0];
     }
 }
